d2ea04bb4803696e0d9b1aed1c3ce4ac745c91c2
ChrisLangsford <langz020@gmail.com>
2015-03-04 15:48:29 +0200

    added log out link for testing, fixed log out routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new'
+    get '/logout' => 'devise/sessions#destroy'
+  end
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
