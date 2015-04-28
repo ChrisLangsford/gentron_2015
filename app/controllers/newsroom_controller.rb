de05175826f4ec75892ee6f93576b2af605b4cb2
ChrisLangsford <langz020@gmail.com>
2015-04-28 15:37:46 +0200

    added industry news feed

--- app/controllers/newsroom_controller.rb
+++ app/controllers/newsroom_controller.rb
@@ -3,5 +3,15 @@ class NewsroomController < ApplicationController
 
 	def index
 		@articles = Article.all
+		urls = %w[http://www.itweb.co.za/index.php?option=com_rd_rss&id=1]
+		feeds = Feedjira::Feed.fetch_and_parse urls
+		feed = feeds["http://www.itweb.co.za/index.php?option=com_rd_rss&id=1"]
+
+		@entries = feed.entries.last(8)
+
+
+
+
+
 	end	
 end
