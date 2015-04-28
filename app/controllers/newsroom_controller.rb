1957d93c3bc56d880dd86f9f2df41fcb357de9be
ChrisLangsford <langz020@gmail.com>
2015-04-28 16:29:38 +0200

    fixed newsroom, removed feed

--- app/controllers/newsroom_controller.rb
+++ app/controllers/newsroom_controller.rb
@@ -3,15 +3,5 @@ class NewsroomController < ApplicationController
 
 	def index
 		@articles = Article.all
-		urls = %w[http://www.itweb.co.za/index.php?option=com_rd_rss&id=1]
-		feeds = Feedjira::Feed.fetch_and_parse urls
-		feed = feeds["http://www.itweb.co.za/index.php?option=com_rd_rss&id=1"]
-
-		@entries = feed.entries.last(8)
-
-
-
-
-
 	end	
 end
