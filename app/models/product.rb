3210817fa2f4660f1b86b5fe55a5246cc7dbd3c4
ChrisLangsford <langz020@gmail.com>
2016-05-23 17:45:25 +0200

    added product picture functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
-searchable do
-  text :name, :boost => 5
-  text :description
-end
+	has_attached_file :picture, :styles => { :medium => "600x230>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
+	searchable do
+		text :name, :boost => 5
+		text :description
+	end
 end
