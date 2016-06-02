4490c6d540f110be58687194ab70d2bc699cbe23
ChrisLangsford <langz020@gmail.com>
2016-06-02 17:22:32 +0200

    added koala and pulled gentron newsfeed from facebook

--- app/controllers/newsroom_controller.rb
+++ app/controllers/newsroom_controller.rb
@@ -1,5 +1,8 @@
 class NewsroomController < ApplicationController
 	def index
 		@articles = Article.all
+		graph = Koala::Facebook::API.new(ENV['FACEBOOK_TOKEN'])
+
+		@articles = graph.get_connection('GentronGroup', 'posts')
 	end
 end
