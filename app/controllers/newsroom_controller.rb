5e4a86c51c54db9c1df9dc7f279adceef2e1609c
ChrisLangsford <langz020@gmail.com>
2016-06-08 09:06:01 +0200

    fixed data link to graph api to work around expiring tokens

--- app/controllers/newsroom_controller.rb
+++ app/controllers/newsroom_controller.rb
@@ -1,8 +1,12 @@
 class NewsroomController < ApplicationController
 	def index
-		@articles = Article.all
-		graph = Koala::Facebook::API.new(ENV['FACEBOOK_TOKEN'])
+		oauth = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'])
+		oauth_token = oauth.get_app_access_token
+		graph = Koala::Facebook::API.new(oauth_token)
+
+		#@articles = Article.all
 
 		@articles = graph.get_connection('GentronGroup', 'posts')
 	end
+
 end
