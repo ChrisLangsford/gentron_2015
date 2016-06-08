f016e1c0496cf2ffafb901c1f4fd453a82fdf85e
ChrisLangsford <langz020@gmail.com>
2016-06-08 10:38:21 +0200

    replaced newsroom with link to facebook

--- app/controllers/newsroom_controller.rb
+++ app/controllers/newsroom_controller.rb
@@ -1,12 +1,4 @@
 class NewsroomController < ApplicationController
 	def index
-		oauth = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'])
-		oauth_token = oauth.get_app_access_token
-		graph = Koala::Facebook::API.new(oauth_token)
-
-		#@articles = Article.all
-
-		@articles = graph.get_connection('GentronGroup', 'posts')
 	end
-
 end
