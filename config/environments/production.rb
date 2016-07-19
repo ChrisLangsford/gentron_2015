f24d407935e2aee09920b8961d9ccb5f88fea774
ChrisLangsford <langz020@gmail.com>
2016-07-19 16:07:44 +0200

    added fix for solr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  Sunspot.config.solr.url = 'http://localhost:8983/solr'
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
