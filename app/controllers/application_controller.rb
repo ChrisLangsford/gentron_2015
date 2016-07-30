7baf13f293f0696042eec43d785986f4c42af5f0
ChrisLangsford <langz020@gmail.com>
2016-07-30 15:08:46 +0200

    removed user crud rights

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SimpleCaptcha::ControllerHelpers
   #before_action :authenticate_user!
-  	def after_sign_in_path_for(resource)
-  		staff_path
-	end
+  def after_sign_in_path_for(resource)
+  	staff_path
+  end
+  def after_sign_out_path_for(resource_or_scope)
+  	root_path
+  end
 end
