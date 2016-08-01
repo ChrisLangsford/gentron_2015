c6495ba8713005a83654f71d59ade5526f05d40d
ChrisLangsford <langz020@gmail.com>
2016-08-01 16:50:46 +0200

    fixed val

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
 	has_attached_file :picture, :styles => { :medium => "600x230>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
-	validates name: presence: true
+	validates :name, presence: true
 
 end
